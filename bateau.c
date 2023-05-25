c4a5923c75afd64b6d265e0e27f3215df1ff8bc3
G-LK <114987519+G-LK@users.noreply.github.com>
2023-05-25 15:57:24 +0200

    all const

--- bateau.c
+++ bateau.c
@@ -10,4 +10,18 @@
 */
 
 #include "bateau.h"
+#include <inttypes.h>
+
+//Taxes
+const double TAXE_BASE_VOILIER 				= 50;
+const double TAXE_BASE_MOTEUR 				= 100;
+const double TAXE_SPECIFIQUE_VOILIER 		= 0;
+const double TAXE_SPECIFIQUE_VOILIER_IF 	= 25;
+const double TAXE_SPECIFIQUE_PECHE 			= 0;
+const double TAXE_SPECIFIQUE_PECHE_IF 		= 100;
+const double TAXE_SPECIFIQUE_PLAISANCE 	= 50;
+const double TAXE_SPECIFIQUE_PLAISANCE_IF	= 15;
+const uint16_t SURFACE_VOILIER 				= 200;
+const uint8_t TONNE_POISSON					= 20;
+const uint16_t PUISSANCE_MOTEUR				= 100;
 
