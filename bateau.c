9f4e13bb51da0ce469ac98ca556c5d8a5949861f
Nathan <nathanarmand@hotmail.com>
2023-05-25 15:32:30 +0200

    Init files

---
+++ bateau.c
@@ -0,0 +1,17 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier :
+ Auteur(s)      :
+ Date creation  :
+ Description    :
+ Remarque(s)    :
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include "bateau.h"
+
+int main(void)
+{
+    return EXIT_SUCCESS
+}
---
+++ bateau.h
@@ -0,0 +1,14 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier :
+ Auteur(s)      :
+ Date creation  :
+ Description    :
+ Remarque(s)    :
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ -----------------------------------------------------------------------------------
+*/
+#ifndef BATEAU_H
+#define BATEAU_H
+
+#endif //BATEAU_H
