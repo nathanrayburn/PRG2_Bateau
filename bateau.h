612de9de4c08d9ade5bd95f412b758ecc910ae78
Nathan <nathanarmand@hotmail.com>
2023-05-25 15:59:58 +0200

    init structure

--- bateau.h
+++ bateau.h
@@ -10,5 +10,33 @@
 */
 #ifndef BATEAU_H
 #define BATEAU_H
+typedef enum {
+    VOILIER, MOTEUR
+} typeBateau;
+static const char *const typeBateauString[] = {
+        "Voilier", "Moteur"
+};
+
+typedef enum {
+    PECHE, PLAISANCE, AUCUNE
+} CasUtilisation;
+static const char *const casUtilisationString[] = {
+        "Peche", "Plaisance", "Aucune"
+};
+typedef struct Voilier {
+
+};
+typedef struct Moteur {
+
+};
+typedef struct CaracteristiqueBateau {
+    Voilier voilier;
+    Moteur moteur;
+};
+typedef struct {
+    char *nomBateau;
+    typeBateau typeBateau;
+    struct CaracteristiqueBateau caracteristiqueBateau;;
+} Bateau;
 
 #endif //BATEAU_H
