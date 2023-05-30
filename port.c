ce6787d19b0dd40d924f36a48ea0aebd789958e5
Nathan Rayburn <nathanrayburn2001@gmail.com>
2023-05-31 00:38:32 +0200

    created port files

---
+++ port.c
@@ -0,0 +1,5 @@
+//
+// Created by nathanrayburn on 31/05/2023.
+//
+
+#include "port.h"
---
+++ port.h
@@ -0,0 +1,8 @@
+//
+// Created by nathanrayburn on 31/05/2023.
+//
+
+#ifndef PRG2_BATEAU_PORT_H
+#define PRG2_BATEAU_PORT_H
+
+#endif //PRG2_BATEAU_PORT_H
